bc68870121544fe60d73c763dbca05f257563f8a
Andrew <arvizumunozandrew@gmail.com>
2024-01-25 15:25:04 -0800

    fixed issue with page not found, moving on to debouncing

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,20 +3,20 @@ import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page({
-    searchParams,
+  searchParams,
 }: {
-    searchParams?: {
-        query?: string;
-        page?: string;
-    };
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
 }) {
-    const query = searchParams?.query || '';
-    const currentPage = Number(searchParams?.page) || 1;
-
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+ 
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -28,7 +28,7 @@ export default async function Page({
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-      </Suspense> 
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
         {/* <Pagination totalPages={totalPages} /> */}
       </div>
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,26 +1,25 @@
 'use client';
-
+ 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-
 
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+ 
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
-
-    if(term) {
+    if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-    replace('${pathname}?${pathname.toString()}');
+    replace(`${pathname}?${params.toString()}`);
   }
-
+ 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
