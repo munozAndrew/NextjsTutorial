3452dc655db2b312e99b240d7d750d413e80dba4
Andrew <arvizumunozandrew@gmail.com>
2024-01-31 11:14:24 -0800

    finished chap-12 update and delete invoices, potential issue - new invoices are placed in 2nd not 1st.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,8 +14,11 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 
 export async function createInvoice(formData: FormData) {
+
   const { customerId, amount, status} = CreateInvoice.parse ({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -34,4 +37,32 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+
+
+  const { customerId, amount, status} = UpdateInvoice.parse ({
+    customerId: formData.get('customerId'), 
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql `
+  UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+  where id = ${id}
+  `;
+
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+
+  };
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
